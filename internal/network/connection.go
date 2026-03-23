c366942365fbf0f034a41ffa4d07fc4bed92dcad
ritikraj26 <ritikraj2699@gmail.com>
2026-03-23 22:34:44 +0530

    parse correlation_id from request

--- internal/network/connection.go
+++ internal/network/connection.go
@@ -1,21 +1,32 @@
 package network
 
 import (
-	"encoding/binary"
 	"fmt"
 	"net"
+
+	"github.com/codecrafters-io/kafka-starter-go/internal/protocol"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
+	// Buffer to read the request
+	requestHeader := protocol.NewRequestHeader()
+	if err := requestHeader.ReadFrom(conn); err != nil {
+		fmt.Println("Error reading request header: ", err)
+		return
+	}
+
 	// Prepare the response
-	response := make([]byte, 8)
-	binary.BigEndian.PutUint32(response[0:4], 0) // message_size: 0
-	binary.BigEndian.PutUint32(response[4:8], 7) // correlation_id: 7
+	response := protocol.NewResponse(requestHeader.CorrelationID())
+	serializedResponse, err := response.Serialize()
+	if err != nil {
+		fmt.Println("Error serializing response: ", err)
+		return
+	}
 
 	// Send the response
-	_, err := conn.Write(response)
+	_, err = conn.Write(serializedResponse)
 	if err != nil {
 		fmt.Println("Error writing response: ", err)
 	}
--- internal/protocol/error_codes.go
+++ internal/protocol/error_codes.go
@@ -3,8 +3,8 @@ package protocol
 // Kafka error codes returned in response headers and bodies.
 // TODO: implement
 const (
-	ErrNone                        int16 = 0
-	ErrUnknownTopicOrPartition     int16 = 3
-	ErrUnsupportedVersion          int16 = 35
-	ErrUnknownTopicID              int16 = 100
+	ErrNone                    int16 = 0
+	ErrUnknownTopicOrPartition int16 = 3
+	ErrUnsupportedVersion      int16 = 35
+	ErrUnknownTopicID          int16 = 100
 )
