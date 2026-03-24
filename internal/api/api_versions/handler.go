cd8ecd96780c556295bb97888226945c270f176f
ritikraj26 <ritikraj2699@gmail.com>
2026-03-24 10:20:05 +0530

    create a function to build body in api versions

--- internal/api/api_versions/handler.go
+++ internal/api/api_versions/handler.go
@@ -1,7 +1,30 @@
 package apiversions
 
-// TODO: implement
-// Handler responds to ApiVersions requests (api_key=18).
-// It returns the list of API keys and min/max versions supported by this broker.
-// Stages: Handle ApiVersions requests, Include DescribeTopicPartitions in ApiVersions,
-//         Include Fetch in ApiVersions, Include Produce in ApiVersions.
+import (
+	"bytes"
+	"encoding/binary"
+)
+
+func BuildBody(errorCode int16) []byte {
+	buf := new(bytes.Buffer)
+
+	// error_code
+	binary.Write(buf, binary.BigEndian, errorCode)
+
+	// COMPACT_ARRAY length: N+1, so 1 entry = 0x02
+	buf.WriteByte(0x02)
+
+	// Entry: ApiVersions (key=18), versions 0-4
+	binary.Write(buf, binary.BigEndian, int16(18)) // api_key
+	binary.Write(buf, binary.BigEndian, int16(0))  // min_version
+	binary.Write(buf, binary.BigEndian, int16(4))  // max_version
+	buf.WriteByte(0x00)                            // TAG_BUFFER for entry
+
+	// throttle_time_ms
+	binary.Write(buf, binary.BigEndian, int32(0))
+
+	// TAG_BUFFER for response
+	buf.WriteByte(0x00)
+
+	return buf.Bytes()
+}
